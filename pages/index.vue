a0197f2196eacc1233684f1fa03480b5de4974d2
Thiago Gentil <thiagogentil499@gmail.com>
2023-10-05 16:36:52 +0000

    feat: newsleter

--- pages/index.vue
+++ pages/index.vue
@@ -16,30 +16,37 @@
         <div class="w-11/12 h-full mx-auto">
             <div class="w-2/3 h-2/3 mx-auto grid grid-cols-3 grid-rows-2 gap-2">
                 <!-- Elemento 1 -->
-                <div class="col-span-1 row-span-1 bg-verde rounded-3xl flex justify-center p-8">
-                    <h1 class="text-branco font-modak text-5xl">crie</h1>
-                    <p></p>
+                <div class="col-span-1 row-span-1 bg-verde rounded-3xl p-8 px-12">
+                    <h1 class="text-branco font-modak text-5xl text-center mb-4">crie</h1>
+                    <p class="font-montserrat text-xl mb-8 text-branco font-medium ">Forneça aos seus segidores todas as informações que eles precisam sobre você a qualquer momento.</p>
                 </div>
 
                 <!-- Elemento 2 -->
-                <div class="col-span-1 row-span-1 bg-laranja rounded-3xl flex justify-center p-8">
-                    <h1></h1>
-                    <p></p>
+                <div class="col-span-1 row-span-1 bg-laranja rounded-3xl p-8  px-12">
+                    <h1 class="text-branco font-modak text-5xl text-center mb-4">Compartilhe</h1>
+                    <p class="font-montserrat text-xl mb-8 text-branco font-medium ">    Basta um clique e todos saberão o que você tem para mostrar! </p>
                 </div>
 
                 <!-- Elemento 3 -->
-                <div class="col-span-1 row-span-2 bg-azul rounded-3xl flex justify-center p-8">
-                    <h1></h1>
-                    <p></p>
+                <div class="col-span-1 row-span-2 bg-azul rounded-3xl p-8 px-12">
+                    <h1 class="text-branco font-modak text-5xl text-center mb-4">Cresça!</h1>
+                    <p class="font-montserrat text-xl mb-8 text-branco font-medium ">    Analize e entenda o seu crescimento online e compreenda melhor o seu público.</p>
+                    <img src="../assets/img/arrow.svg" alt="" class="h-1/2 mx-auto">
                 </div>
 
                 <!-- Elemento 4 -->
-                <div class="col-span-2 row-span-1 bg-roxo rounded-3xl flex justify-center p-8">
-                    <h1></h1>
-                    <p></p>
+                <div class="col-span-2 row-span-1 bg-roxo rounded-3xl p-8 px-12">
+                    <h1 class="text-branco font-modak text-5xl mb-4">Cresça!</h1>
+                    <p class="font-montserrat text-xl mb-8 text-branco font-medium ">   Dê um quack rapidinho, customizando e organizando todos os seus links e informações com muito estilo e personalidade.</p>
                 </div>
             </div>
         </div>
-
+    </section>
+    <section class="w-full h-96 bg-azul pt-16">
+        <h1 class="w-1/2 mx-auto font-modak text-8xl mb-8 text-branco text-center">Facilite sua carreira virtual hoje!</h1>
+        <form action="" class="w-1/2 mx-auto">
+            <input class="h-16 w-96 rounded-2xl" type="text">
+            <button class=" rounded-2xl bg-verde font-modak text-5xl p-2 text-branco" type="submit">Quack!</button>
+        </form>
     </section>
 </template>
