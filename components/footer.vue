e1f88b669aa2a24bc6563d827f83bfc613db945c
Thiago Gentil <thiagogentil499@gmail.com>
2023-10-06 12:13:31 -0300

    cards

--- components/footer.vue
+++ components/footer.vue
@@ -1,8 +1,24 @@
 <template>
-    <footer class="bg-branco h-40 py-16">
-        <div class="w-11/12 h-20 mx-auto flex justify-between items-center my-auto px-8 rounded-full bg-roxo">
-            <a class="font-montserrat text-2xl text-branco" href="/">Inicio</a>
-            <h1 class="font-modak text-4xl text-branco">Quack</h1>
-        </div>
+    <footer class="bg-branco py-8 relative overflow-hidden">
+      <div class="w-11/12 h-52 mx-auto flex justify-between items-center my-auto px-8 rounded-full bg-roxo relative">
+        <h1 class="font-modak text-9xl absolute left-0 right-0 transition-transform duration-500 animation-slide-infinite">Quack</h1>
+      </div>
     </footer>
-</template>
+  </template>
+  
+  <style scoped>
+  @keyframes slide {
+      0%, 100% {
+          transform: translateX(3%);
+          color: #D9D9D9 /* Defina a cor inicial aqui */
+      }
+      50% {
+          transform: translateX(75%);
+          color: #FFD900; /* Defina a cor da etapa 2 aqui */
+      }
+  }
+  .animation-slide-infinite {
+      animation: slide 5s infinite;
+  }
+  </style>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -15,39 +15,77 @@
                 <img src="../assets/img/colecao.png" alt="Imagem de um celular com o site do quack aberto" class="">
             </div>
         </section>
-        <section class="bg-branco w-full h-screen pt-28">
+        <section class="bg-branco w-full pb-28 pt-28">
             <div class="w-11/12 h-full mx-auto ">
                 <div class="w-4/6 h-4/6 mx-auto grid grid-cols-3 grid-rows-2 gap-2">
-                    <!-- Elemento 1 -->
-                    <div class="col-span-1 row-span-1 bg-verde rounded-3xl p-8 px-12">
-                        <h1 class="text-branco font-modak text-5xl text-center mb-4">crie</h1>
-                        <p class="font-montserrat text-xl mb-8 text-branco font-medium ">Forneça aos seus segidores todas as
-                            informações que eles precisam sobre você a qualquer momento.</p>
+
+                    <div class="col-span-1 row-span-1 flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front bg-verde rounded-3xl p-8 px-12">
+                                <h1 class="text-branco font-modak text-5xl text-center mb-4">crie</h1>
+                                <p class="font-montserrat text-xl mb-8 text-branco font-medium">Forneça aos seus seguidores
+                                    todas as
+                                    informações que eles precisam sobre você a qualquer momento.</p>
+                            </div>
+                            <div class="flip-card-back bg-azul rounded-3xl p-8 px-12">
+                                <!-- Conteúdo da face traseira com um botão -->
+                                <h1 class="text-branco font-modak text-5xl text-center mb-4">Botão na face traseira</h1>
+                                <button
+                                    class="rounded-2xl bg-verde font-modak text-5xl p-2 px-8 text-branco transform-gpu origin-right focus:outline-none transition-transform duration-300 ease-in-out focus:scale-110 focus:rotate-12">Clique</button>
+                            </div>
+                        </div>
                     </div>
 
                     <!-- Elemento 2 -->
-                    <div class="col-span-1 row-span-1 bg-laranja rounded-3xl p-8  px-12">
-                        <h1 class="text-branco font-modak text-5xl text-center mb-4">Compartilhe</h1>
-                        <p class="font-montserrat text-xl mb-8 text-branco font-medium "> Basta um clique e todos saberão o
-                            que você tem para mostrar! </p>
+                    <div class="col-span-1 row-span-1 flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front bg-laranja rounded-3xl p-8 px-12">
+                                <h1 class="text-branco font-modak text-5xl text-center mb-4">Compartilhe</h1>
+                                <p class="font-montserrat text-xl mb-8 text-branco font-medium">Basta um clique e todos
+                                    saberão
+                                    o que você tem para mostrar!</p>
+                            </div>
+                            <div class="flip-card-back bg-roxo rounded-3xl p-8 px-12">
+                                <!-- Conteúdo da face traseira com um botão -->
+                                <h1 class="text-branco font-modak text-5xl text-center mb-4">Botão na face traseira</h1>
+                                <button
+                                    class="rounded-2xl bg-laranja font-modak text-5xl p-2 px-8 text-branco transform-gpu origin-right focus:outline-none transition-transform duration-300 ease-in-out focus:scale-110 focus:rotate-12">Clique</button>
+                            </div>
+                        </div>
                     </div>
+
 
                     <!-- Elemento 3 -->
-                    <div class="col-span-1 row-span-2 bg-azul rounded-3xl p-8 px-12">
-                        <h1 class="text-branco font-modak text-5xl text-center mb-4">Cresça!</h1>
-                        <p class="font-montserrat text-xl mb-8 text-branco font-medium "> Analize e entenda o seu
-                            crescimento online e compreenda melhor o seu público.</p>
-                        <img src="../assets/img/arrow.svg" alt="" class="h-1/2 mx-auto">
+                    <div class="col-span-1 row-span-2 flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front bg-azul rounded-3xl p-8 px-12">
+                                <h1 class="text-branco font-modak text-5xl text-center mb-4">Cresça!</h1>
+                                <p class="font-montserrat text-xl mb-8 text-branco font-medium">Analise e entenda o seu
+                                    crescimento online e compreenda melhor o seu público.</p>
+                                <img src="../assets/img/arrow.svg" alt="" class="h-1/2 mx-auto">
+                            </div>
+                            <div class="flip-card-back bg-verde rounded-3xl p-8 px-12">
+                                <!-- Conteúdo da face traseira com um botão -->
+                                <h1 class="text-branco font-modak text-5xl text-center mb-4">Botão na face traseira</h1>
+                                <button
+                                    class="rounded-2xl bg-azul font-modak text-5xl p-2 px-8 text-branco transform-gpu origin-right focus:outline-none transition-transform duration-300 ease-in-out focus:scale-110 focus:rotate-12">Clique</button>
+                            </div>
+                        </div>
                     </div>
 
+
                     <!-- Elemento 4 -->
-                    <div class="col-span-2 row-span-1 bg-roxo rounded-3xl p-8 px-12">
-                        <h1 class="text-branco font-modak text-5xl mb-4">Customize</h1>
-                        <p class="font-montserrat text-xl mb-4 text-branco font-medium "> Dê um quack rapidinho,
-                            customizando e organizando todos os seus links e informações com muito estilo e personalidade.
-                        </p>
-                        <img src="../assets/img/cardClick.png" alt="" class="w-8/12 mx-auto">
+                    <div class="col-span-2 row-span-1 bg-roxo rounded-3xl p-8 px-12 flip-card">
+                        <div class="flip-card-inner">
+                            <h1 class="text-branco font-modak text-5xl mb-4">Customize</h1>
+                            <p class="font-montserrat text-xl mb-4 text-branco font-medium">Dê um quack rapidinho,
+                                customizando e organizando todos os seus links e informações com muito estilo e
+                                personalidade.
+                            </p>
+                            <img src="../assets/img/cardClick.png" alt="" class="w-8/12 mx-auto">
+                        </div>
                     </div>
+
                 </div>
             </div>
         </section>
@@ -64,6 +102,38 @@
         <Footer />
     </div>
 </template>
+
+
+<style scoped>
+.flip-card {
+    perspective: 1000px;
+    width: 100%;
+    height: 100%;
+}
+
+.flip-card-inner {
+    width: 100%;
+    height: 100%;
+    transition: transform 0.5s;
+    transform-style: preserve-3d;
+}
+
+.flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+}
+
+.flip-card-front,
+.flip-card-back {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    backface-visibility: hidden;
+}
+
+.flip-card-back {
+    transform: rotateY(180deg);
+}
+</style>
 
 <script>
 export default {
